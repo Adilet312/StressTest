07f72e68cfb46c46f45f48c26e0a60572b94d3c0
Adilet Momunaliev, Nina Potrebich and Wei Dai <nina@>
2019-10-29 11:54:27 -0700

    Creating quiz with Jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
+  $("form#form1").submit(function(event) {
+    event.preventDefault();
+    var sum = 0;
 
+    $("input:checkbox[name=question1]:checked").each(function() {
+      sum += 1;
+    });
+
+    $("input:checkbox[name=question2]:checked").each(function() {
+      sum += 2;
+    });
+
+    $("input:checkbox[name=question3]:checked").each(function() {
+      sum -= 1;
+    });
+
+    if (sum <= 0) {
+      $("#result").text("low");
+    } else if ( sum < 4) {
+        $("#result").text("medium");
+    } else {
+        $("#result").text("high");
+    }
+
+     $(".resultClass").show();
+  });
 
 
 });
